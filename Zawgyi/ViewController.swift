a8482821a9e8b249b571814f7f71b216104fd7b6
Ye Myat Min <yemyat@gmail.com>
2014-06-10 20:57:21 +0800

    Initial Commit

---
+++ Zawgyi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zawgyi
+//
+//  Created by Ye Myat Min on 10/6/14.
+//  Copyright (c) 2014 Nex Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
